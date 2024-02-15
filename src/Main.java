dfc8a865daccf1fe1fd4da9dae57d04d9c4e16d6
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-15 18:13:38 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import java.util.Scanner; //importing Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in); //naming Scanner
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        boolean going = true; //looping variable
+
+        do {
+            System.out.println("Enter a measurement (In meters) to be converted into miles, feet, and inches."); //asking user to input number
+
+            if (in.hasNextDouble()) //statement detecting if user entered a double
+            {
+                double meters = in.nextDouble(); //defining meters
+
+                double miles = (meters / 1609); //calculating miles
+
+                double feet = (meters * 3.281); //calculating feet
+
+                double inches = (feet * 12); //calculating inches
+
+                System.out.println(meters + " meters converted into miles is " + miles + " miles, converted into feet is " + feet + " feet, and converted into inches is " + inches + " inches."); //outputting all calculations
+
+                going = false; //ending loop
+            }
+            else if (!in.hasNextDouble()) //user didn't input a measurement
+            {
+                String trash = in.nextLine(); //defining variable trash
+                
+                System.out.println(trash + " is not a measurement! Try again!"); //telling user to re-type
+            }
+        }while (going); //loops while going = true
     }
 }
